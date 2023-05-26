4022672d56083950de205f7b1a27d2a8de0cf342
tech-hunter <128301154+creativekushwahaji01@users.noreply.github.com>
2023-05-26 14:01:22 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function convertWeight() {
-    const div = document.getElementById('f1').value;
-    const earthWeight = document.getElementById('ew').value;
-    const planetGravity = document.getElementById('planet-select').value;
-    const result = earthWeight/div* planetGravity;
-    
-    document.getElementById('result').textContent = `Your weight on this planet is ${result.toFixed(2)} kg.`;
-  }
